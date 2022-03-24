0a47e061a65bf7fc8ff5a57dc46f9a6ce70449a4
Sebastian <hvber01@gmail.com>
2022-03-24 19:24:28 +0100

    Update license in footer

--- src/components/molecules/Footer.js
+++ src/components/molecules/Footer.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import styled from 'styled-components'
 
 export default function Footer() {
     return (
         <StyledFooter>
-            <p className='copyright'>&copy; Copyright Infonite. Wszelkie prawa zastrzeżone.</p>
-            <p>Infonite to aplikacja, która nie jest w żaden sposób wspierana przez Epic Games.</p>
-            <p>Epic Games, Fortnite i wszystkie powiązane właściwości są znakami towarowymi lub zastrzeżonymi znakami towarowymi firmy Epic Games, Inc.</p>
+            <p className='copyright'>Oparte na licencji <a href='https://github.com/sebastianhaber/infonite' rel='noreferrer' target='_blank' className="bold blue">MIT</a></p>
+            <div className="small">
+                <p>Infonite to aplikacja, która nie jest w żaden sposób wspierana przez Epic Games.</p>
+                <p>Epic Games, Fortnite i wszystkie powiązane właściwości są znakami towarowymi lub zastrzeżonymi znakami towarowymi firmy Epic Games, Inc.</p>
+            </div>
         </StyledFooter>
     )
 }
@@ -17,13 +20,11 @@ const StyledFooter = styled.footer`
     padding: 2rem;
     padding-bottom: 6rem;
     text-align: center;
-    font-size: clamp(12px, 0.75vw, 2vw);
     div{
         max-width: 1920px;
         margin: 0 auto;
     }
     .copyright{
         margin-bottom: 1rem;
-        font-size: 1vw;
     }
 `;
